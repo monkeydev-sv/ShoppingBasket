060156e5c75d82d2d492afb454e804798827c466
christer Holm <ch@star78.se>
2021-09-21 08:58:10 +0200

    passing two items new metod

--- src/test/java/ShoppingBasketTest.java
+++ src/test/java/ShoppingBasketTest.java
@@ -15,10 +15,15 @@ assertEquals(0.0, basket.getTotal(),0.0);
     }
    @Test
     public void totalOfSingleItem(){
-     ShoppingBasket basket = new ShoppingBasket(Arrays.asList(new Item(100.0,1)));
+     ShoppingBasket basket = BuildBasketWithItems();
     assertEquals(100.0, basket.getTotal(),0.0);
     }
-@Test
+
+    private ShoppingBasket BuildBasketWithItems() {
+        return new ShoppingBasket(Arrays.asList(new Item(100.0, 1)));
+    }
+
+    @Test
 public void totalOfTwoItems(){
         ShoppingBasket basket = new ShoppingBasket(Arrays.asList(new Item(100.0,1),new Item(200, 1)));
     assertEquals(300.0, basket.getTotal(),0.0);
